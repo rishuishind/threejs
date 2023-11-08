59bf9328d2a1eb06d25c05543fa271ae7028afb9
Rishabh Sharma <hespnod2348@gmail.com>
2023-11-08 17:32:41 +0530

    made changes in shirt to easing the color

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -12,8 +12,14 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
+  useFrame((state,delta)=>easing.dampC(materials.lambert1.color,
+    snap.color,0.25,delta
+    ))
+
+    const stateString = JSON.stringify(snap);
+
   return (
-    <group>
+    <group key={stateString}>
         <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
@@ -21,6 +27,23 @@ const Shirt = () => {
         material_roughness={1}
         dispose={null}
         >
+            {snap.isFullTexture &&(
+                <Decal
+                position={[0,0,0]}
+                rotation={[0,0,0]}
+                scale={1}
+                map={fullTexture}
+                />
+            )}
+
+            {snap.isLogoTexture &&(
+                <Decal
+                position={[0,0.004,0.15]}
+                rotation={[0,0,0]}
+                scale={0.15}
+                map={logoTexture}
+                />
+            )}
 
         </mesh>
     </group>
