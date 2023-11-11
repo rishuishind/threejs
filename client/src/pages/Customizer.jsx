97c847a35f8bb2df596ce8f7ae10c59e1c323546
Rishabh Sharma <hespnod2348@gmail.com>
2023-11-11 17:22:55 +0530

    added functionality to change the color

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -11,6 +11,33 @@ import {ColorPicker,FilePicker,AIPicker,Tab,CustomButton} from '../components'
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  const [file,setFile] = useState('');
+
+  const [prompt,setPrompt] = useState('');
+  const [generatingImg,setGeneratingImg] = useState(false);
+
+  const [activeEditorTab,setActiveEditorTab] = useState('');
+  const [activeFilterTab,setActiveFilterTab] = useState({
+    logoShirt:true,
+    stylishShirt:false,
+  })
+
+  // Show tab content depending upon the current active Tab
+
+  const generateTabContent = ()=>{
+    switch(activeEditorTab){
+    case 'colorpicker':
+      return <ColorPicker/>
+    case 'filepicker':
+      return <FilePicker/> 
+    case 'aipicker':
+      return <AIPicker/>
+    default:
+      return null;
+    }
+  }
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -25,9 +52,10 @@ const Customizer = () => {
                 <Tab
                 key={tab.name}
                 tab={tab}
-                handleClick={()=>{}}
+                handleClick={()=>{setActiveEditorTab(tab.name)}}
                 />
               ))}
+              {generateTabContent()}
             </div>
           </div>
         </motion.div>
